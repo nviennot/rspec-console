2f4b7d06fc8546868d6d261a84fc28237ac0315a
Nicolas Viennot <nicolas@viennot.biz>
2012-12-21 18:13:36 -0500

    reset method for CucumberConsole

--- lib/rspec-console/runner.rb
+++ lib/rspec-console/runner.rb
@@ -1,27 +1,30 @@
 class RSpecConsole::Runner
+  def self.reset(args)
+    require 'rspec'
+
+    if Gem.loaded_specs['rspec'].version < Gem::Version.new('2.9.10')
+      raise 'Please use RSpec 2.9.10 or later'
+    end
+
+    ::RSpec::Core::Runner.disable_autorun!
+    ::RSpec::Core::Configuration.class_eval { define_method(:command) { 'rspec' } }
+    ::RSpec.reset
+
+    self.config_cache.cache do
+      ::RSpec.configure do |config|
+        config.output_stream = STDOUT
+        config.color_enabled = true
+      end
+
+      require "./spec/spec_helper"
+    end
+  end
+
   def self.run(args)
     require 'rails-env-switcher'
 
     RailsEnvSwitcher.with_env('test', :reload => true) do
-      require 'rspec'
-
-      if Gem.loaded_specs['rspec'].version < Gem::Version.new('2.9.10')
-        raise 'Please use RSpec 2.9.10 or later'
-      end
-
-      ::RSpec::Core::Runner.disable_autorun!
-      ::RSpec::Core::Configuration.class_eval { define_method(:command) { 'rspec' } }
-      ::RSpec.reset
-
-      self.config_cache.cache do
-        ::RSpec.configure do |config|
-          config.output_stream = STDOUT
-          config.color_enabled = true
-        end
-
-        require "./spec/spec_helper"
-      end
-
+      reset(args)
       ::RSpec::Core::CommandLine.new(args).run(STDERR, STDOUT)
     end
   end
